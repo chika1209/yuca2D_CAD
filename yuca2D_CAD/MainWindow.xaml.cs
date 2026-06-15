bc00f56e389728ac1f4e98aa0314ca9affd35c45
cyuka <cyuka@CHIKA1209>
2026-06-15 18:15:10 +0900

    線を描画する

--- yuca2D_CAD/MainWindow.xaml.cs
+++ yuca2D_CAD/MainWindow.xaml.cs
@@ -16,8 +16,39 @@ namespace yuca2D_CAD;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private Point? startPoint = null;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void DrawingCanvas_MouseLeftButtonDown(
+        object sender,
+        MouseButtonEventArgs e)
+    {
+        Point p = e.GetPosition(DrawingCanvas);
+
+        if (startPoint == null)
+        {
+            startPoint = p;
+        }
+        else
+        {
+            Line line = new Line();
+
+            line.X1 = startPoint.Value.X;
+            line.Y1 = startPoint.Value.Y;
+
+            line.X2 = p.X;
+            line.Y2 = p.Y;
+
+            line.Stroke = Brushes.Black;
+            line.StrokeThickness = 2;
+
+            DrawingCanvas.Children.Add(line);
+
+            startPoint = null;
+        }
     }
 }
