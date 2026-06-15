6f1557e24b9dc94d04688f15824e1883a7478919
cyuka <cyuka@CHIKA1209>
2026-06-15 17:19:48 +0900

    プロジェクトファイルを追加。

---
+++ yuca2D_CAD/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace yuca2D_CAD;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
